a4b0f52e6f87f67f7b6554ee9fa5496bb1863e6d
firebaserecipes <firebaserecipes@gmail.com>
2020-11-25 14:05:25 -0700

    Add Firebase Auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useState } from "react";
+import FirebaseAuthService from "./FirebaseAuthService";
+import LoginForm from "./components/LoginForm";
+
 import "./App.scss";
 
-// eslint-disable-next-line no-unused-vars
-import firebase from "./FirebaseConfig";
-
 function App() {
+  const [user, setUser] = useState(null);
+
+  FirebaseAuthService.subscribeToAuthChanges(setUser);
+
   return (
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
+        <LoginForm existingUser={user}></LoginForm>
       </div>
     </div>
   );
